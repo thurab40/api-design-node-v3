8ddb0ce9eefcecdddfec6b168d6fd361cd5a8b54
thurab40 <acamarian66@gmail.com>
2020-03-04 00:26:25 +0000

    lesson 4 done

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,20 @@
-export default {}
+import { mongoose } from 'mongoose'
+
+import { Item } from './item.model'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  console.log(item)
+}
+
+run()
+
+// export default {}
--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,44 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'complete', 'pastdue'],
+      default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+// a compund index to ensure all tasks in a list have unique names
+itemSchema.index(
+  {
+    list: 1, // the 1 here mean the sorting order - here order matters - it will go to list and so
+    name: 1
+  },
+  {
+    unique: true
+  }
+)
+
 export const Item = mongoose.model('item', itemSchema)
