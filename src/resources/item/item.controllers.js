b46d40f65bc058c1bad891a4ef49badacde77667
thurab40 <acamarian66@gmail.com>
2020-03-04 02:03:50 +0000

    lesson 4 up

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,6 @@
-export default {}
+import { crudControllers } from '../../utils/crud'
+import { Item } from './item.model'
+
+export default crudControllers(Item)
+
+// you can overite these controllers see the transcript fro wiring up crud controllers
